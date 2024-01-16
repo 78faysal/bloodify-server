e485df9d5861e4b2f6c1ab89920e81ce4158cd70
78faysal <faysal7830@gmail.com>
2024-01-17 01:12:23 +0600

    user modified

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jq69c8i.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,11 +29,43 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const donorCollection = client.db('Bloodify').collection('donors');
+    const userCollection = client.db('Bloodify').collection('users');
 
-    app.post('/donors', async(req, res) => {
-        const donor = req.body;
-        const result = await donorCollection.insertOne(donor);
+    app.get('/users/:email', async(req, res) => {
+        const email = req.params.email;
+        const filter = {email: email};
+        const user = await userCollection.findOne(filter);
+        res.send(user);
+    })
+    
+    app.patch('/users/:email', async(req, res) => {
+        const email = req.params.email;
+        const updatedInfo = req.body;
+        const {name, image, blood, division, district, password} = updatedInfo;
+        // console.log(updatedInfo);
+        const filter = {email: email};
+        const updatedDoc = {
+            $set: {
+                name,
+                image,
+                blood,
+                division,
+                district,
+                password
+            }
+        }
+        const user = await userCollection.updateOne(filter, updatedDoc);
+        res.send(user);
+    })
+
+    app.get('/users', async(req, res) => {
+        const result = await userCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.post('/users', async(req, res) => {
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
         res.send(result);
     })
 
