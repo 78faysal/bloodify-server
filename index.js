872a5b1df5256329f2c33ecc747637d59a85e696
78faysal <faysal7830@gmail.com>
2024-01-17 08:37:41 +0600

    donation request created

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config();
+require("dotenv").config();
 
-
-// middlewares 
+// middlewares
 app.use(cors());
 app.use(express.json());
 
-
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jq69c8i.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,52 +25,76 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const userCollection = client.db('Bloodify').collection('users');
+    const userCollection = client.db("Bloodify").collection("users");
+    const donationRequestCollection = client
+      .db("Bloodify")
+      .collection("donation_requests");
 
-    app.get('/users/:email', async(req, res) => {
-        const email = req.params.email;
-        const filter = {email: email};
-        const user = await userCollection.findOne(filter);
-        res.send(user);
-    })
-    
-    app.patch('/users/:email', async(req, res) => {
-        const email = req.params.email;
-        const updatedInfo = req.body;
-        const {name, image, blood, division, district, password} = updatedInfo;
-        // console.log(updatedInfo);
-        const filter = {email: email};
-        const updatedDoc = {
-            $set: {
-                name,
-                image,
-                blood,
-                division,
-                district,
-                password
-            }
-        }
-        const user = await userCollection.updateOne(filter, updatedDoc);
-        res.send(user);
-    })
+    // users api
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const user = await userCollection.findOne(filter);
+      res.send(user);
+    });
 
-    app.get('/users', async(req, res) => {
-        const result = await userCollection.find().toArray();
+    app.patch("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const updatedInfo = req.body;
+      const { name, image, blood, division, district, password } = updatedInfo;
+      // console.log(updatedInfo);
+      const filter = { email: email };
+      const updatedDoc = {
+        $set: {
+          name,
+          image,
+          blood,
+          division,
+          district,
+          password,
+        },
+      };
+      const user = await userCollection.updateOne(filter, updatedDoc);
+      res.send(user);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // donation requests api
+    app.get("/donation_requests", async (req, res) => {
+      const requestLength = parseInt(req.query.length);
+      console.log(requestLength);
+      if (requestLength) {
+        const allRequests = await donationRequestCollection.find().toArray();
+        const neededData = allRequests.slice(0, requestLength);
+        // console.log(neededData.length);
+        res.send(neededData);
+      } else {
+        const result = await donationRequestCollection.find().toArray();
         res.send(result);
-    })
+      }
+    });
 
-    app.post('/users', async(req, res) => {
-        const user = req.body;
-        const result = await userCollection.insertOne(user);
-        res.send(result);
-    })
-
-
-
+    app.post("/donation_requests", async (req, res) => {
+      const reqestData = req.body;
+      const result = await donationRequestCollection.insertOne(reqestData);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -82,13 +102,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('Blodify server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Blodify server is running");
+});
 
 app.listen(port, () => {
-    console.log('Server running on port', port);
-})
+  console.log("Server running on port", port);
+});
